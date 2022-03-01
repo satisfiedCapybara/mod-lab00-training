2ab2f1571df20ff0fb0466cc6ff53df0f5c28ef3
anton <ashtanyuk@gmail.com>
2022-03-01 09:24:46 +0300

    Начальный коммит

---
+++ include/fun.h
@@ -0,0 +1,7 @@
+// Copyright 2022 UNN-IASR
+#ifndef INCLUDE_FUN_H_
+#define INCLUDE_FUN_H_
+
+long long power(long long x, unsigned short n);
+
+#endif  // INCLUDE_FUN_H_
---
+++ src/fun.cpp
@@ -0,0 +1,6 @@
+// Copyright 2022 NNTU-CS
+#include "fun.h"
+
+long long power(long long x, unsigned short n) {
+    return 0;
+}
